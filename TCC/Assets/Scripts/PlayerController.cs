e9a58d3df447048371f66431ed94cd23b45b0441
Keveny de Araújo Bezerra Lucio <keveny.l@academico.ifrn.edu.br>
2023-07-18 11:54:40 -0300

    commit

--- TCC/Assets/Scripts/PlayerController.cs
+++ TCC/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,16 +9,38 @@ public class PlayerController : MonoBehaviour
     public float _playerVelocidade;
     private Vector2 _playerDireção;
 
+    public GameObject tiroDoPlayer;
+    public Transform localDoDisparo;
+    public bool temTiroDuplo;
+
     // Start is called before the first frame update
     void Start()
     {
         _playerRigidbody2D = GetComponent<Rigidbody2D>();
+        temTiroDuplo = false;
     }
 
     // Update is called once per frame
     void Update()
     {
         _playerDireção = new Vector2(Input.GetAxisRaw("Horizontal"),Input.GetAxisRaw("Vertical"));
+        Atirar();
     }
-    
+
+    void FixedUpdate()
+    {
+        _playerRigidbody2D.MovePosition(_playerRigidbody2D.position + _playerDireção * _playerVelocidade * Time.fixedDeltaTime);
+    }
+
+    private void Atirar()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (temTiroDuplo == false)
+            {
+                Instantiate(tiroDoPlayer, localDoDisparo.position, localDoDisparo.rotation );
+            }
+            
+        }
+    }
 }
